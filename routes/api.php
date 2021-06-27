30de83ff3a8116b4160318533e0a22082c36d1a8
Leone Oliveira <falecom@leone.tec.br>
2021-06-27 09:02:41 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,13 @@
 <?php
 
 require __DIR__.'/api/v1/default.php';
+  
+$router->post('/api{}', [
+  function (){
+    return new Response(404, ['error' => 404, 'message' => 'Não encontrado'], 'application/json');
+  }]);
+
+$router->get('/api{}', [
+  function (){
+    return new Response(404, ['error' => 404, 'message' => 'Não encontrado'], 'application/json');
+  }]);
