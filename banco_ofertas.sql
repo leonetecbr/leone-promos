21525a2c4769f79630fe1cd442fdcf535be17b8b
Leone Oliveira <falecom@leone.tec.br>
2021-07-02 13:13:57 -0300

    Add database file

---
+++ banco_ofertas.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 02/07/2021 às 13:09
+-- Versão do servidor: 5.7.23-23
+-- Versão do PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "-03:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `*************`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `newsletter`
+--
+
+CREATE TABLE `newsletter` (
+  `hash` varchar(32) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `verifed` tinyint(1) NOT NULL DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `notify`
+--
+
+CREATE TABLE `notify` (
+  `endpoint` varchar(256) NOT NULL,
+  `p256dh` varchar(88) NOT NULL,
+  `auth` varchar(24) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `newsletter`
+--
+ALTER TABLE `newsletter`
+  ADD PRIMARY KEY (`hash`);
+
+--
+-- Índices de tabela `notify`
+--
+ALTER TABLE `notify`
+  ADD PRIMARY KEY (`endpoint`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
