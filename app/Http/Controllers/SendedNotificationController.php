c417f1e14670288b5a031277a2d1f6bc87b54162
Leone Oliveira <falecom@leone.tec.br>
2021-12-21 12:05:58 -0300

    Ordenação das notificação anteriores pelas mais recentes

--- app/Http/Controllers/SendedNotificationController.php
+++ app/Http/Controllers/SendedNotificationController.php
@@ -8,7 +8,7 @@ class SendedNotificationController extends Controller
 {
   public function get($page = 1)
   {
-    $sended_notifications = SendedNotification::paginate(10, '*', 'page', $page);
+    $sended_notifications = SendedNotification::orderBy('sended_at', 'DESC')->paginate(10, '*', 'page', $page);
     $notifications = $sended_notifications->items();
     return view('admin.sended_notifications', ['notifications' => $notifications, 'final' => $sended_notifications->lastPage(), 'page' => $page, 'group_name' => 1]);
   }
